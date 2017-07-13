4507b2a596d91236596d731662acbfae4745eedd
fajarmduro <fajarmduro@gmail.com>
2017-07-13 23:07:51 +0700

    Update saflinkupdate01.js

--- saflinkupdate01.js
+++ saflinkupdate01.js
@@ -23,4 +23,10 @@ var x=Math.floor((Math.random()*3)+ 1);
 var xxx=null;
 if(x=="1"){xxx="start-download-here.html"}
 if(x=="2"){xxx="download-insure-buy-general-insurance.html"}
-if(x=="3"){xxx="download-ask-lawyer-legal-help-apk-for.html"}a_to_ve[i].href="http://confighisshgratis.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;rel="nofollow";}}
+if(x=="3"){xxx="download-ask-lawyer-legal-help-apk-for.html"}a_to_ve[i].href="http://confighisshgratis.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
+{a_to_vj.innerHTML+=a_to_vb;}
+if(a_to_vk)
+{a_to_vk.innerHTML+=a_to_va;}}
+function a_to_fa()
+{var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
